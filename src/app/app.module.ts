af79f2845fb48b95e37cc2a01da75a84873fce47
Justin <justinm0979@gmail.com>
2020-03-13 15:04:38 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { InputComponent } from "./input/input.component";
+import { CardFormComponent } from "./card-form/card-form.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, InputComponent, CardFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { DateFormControl } from "../date-form-control";
+
+@Component({
+  selector: "app-card-form",
+  templateUrl: "./card-form.component.html",
+  styleUrls: ["./card-form.component.css"]
+})
+export class CardFormComponent implements OnInit {
+  cardForm = new FormGroup({
+    cardName: new FormControl("", [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    cardNumber: new FormControl("", [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    cardExpiration: new DateFormControl("", [
+      Validators.required,
+      Validators.pattern(/^([0-1]?\d{1})\/\d{2}$/)
+    ]),
+    cardSecurityCode: new FormControl("", [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+      Validators.pattern(/^\d{3}$/)
+    ])
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.cardForm.value);
+  }
+}
